9f873c68bb755481e3f79daf2079dde97073cae4
唐加彬 <me@tangjiabin.cn>
2025-07-24 09:36:47 +0800

    添加分页响应结构体

--- src/types.rs
+++ src/types.rs
@@ -1,3 +1,14 @@
+use serde::Serialize;
+
 pub use crate::macros::ApiRes;
 pub type ApiErr = ApiRes<()>;
 pub type ApiResult<T> = Result<ApiRes<T>, ApiErr>;
+pub type ApiPageRes<T> = ApiRes<PageRes<T>>;
+
+#[derive(Serialize, Debug)]
+pub struct PageRes<T> {
+    pub page: u64,
+    pub per_page: u64,
+    pub total: u64,
+    pub list: Vec<T>,
+}

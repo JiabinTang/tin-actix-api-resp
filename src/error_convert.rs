dc270bc431762bbb9b2718691c8ccae2a3ccc408
唐加彬 <me@tangjiabin.cn>
2025-07-29 16:22:15 +0800

    修复警告

--- src/error_convert.rs
+++ src/error_convert.rs
@@ -6,16 +6,14 @@ impl std::fmt::Display for ApiErr {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
-            "ApiErr: code={}, message={}",
-            self.code(),
-            self.message()
+            "ApiErr: code={{self.code()}}, message={{self.message()}}"
         )
     }
 }
 
 impl From<std::io::Error> for ApiErr {
     fn from(err: std::io::Error) -> Self {
-        log::error!(target: "tin_actix_api_resp::error", "[IO] std::io::Error: {:#?}", err);
+        log::error!(target: "tin_actix_api_resp::error", "[IO] std::io::Error: {err:#?}");
         ApiErr::InternalServerError("Internal server error".into())
     }
 }
@@ -23,7 +21,7 @@ impl From<std::io::Error> for ApiErr {
 #[cfg(feature = "sea-orm")]
 impl From<sea_orm::DbErr> for ApiErr {
     fn from(err: sea_orm::DbErr) -> Self {
-        log::error!(target: "tin_actix_api_resp::error", "[SeaORM] DbErr: {:#?}", err);
+        log::error!(target: "tin_actix_api_resp::error", "[SeaORM] DbErr: {err:#?}");
         ApiErr::InternalServerError("Internal server error".into())
     }
 }
@@ -34,7 +32,7 @@ impl From<sea_orm::TransactionError<ApiErr>> for ApiErr {
         match err {
             sea_orm::TransactionError::Transaction(err) => err,
             sea_orm::TransactionError::Connection(err) => {
-                log::error!(target: "tin_actix_api_resp::error", "[SeaORM] TransactionError::Connection: {:#?}", err);
+                log::error!(target: "tin_actix_api_resp::error", "[SeaORM] TransactionError::Connection: {err:#?}");
                 ApiErr::InternalServerError("Internal server error".into())
             }
         }
@@ -44,7 +42,7 @@ impl From<sea_orm::TransactionError<ApiErr>> for ApiErr {
 #[cfg(feature = "redis")]
 impl From<redis::RedisError> for ApiErr {
     fn from(err: redis::RedisError) -> Self {
-        log::error!(target: "tin_actix_api_resp::error", "[Redis] RedisError: {:#?}", err);
+        log::error!(target: "tin_actix_api_resp::error", "[Redis] RedisError: {err:#?}");
         ApiErr::InternalServerError("Internal server error".into())
     }
 }
@@ -52,7 +50,7 @@ impl From<redis::RedisError> for ApiErr {
 #[cfg(feature = "serde_json")]
 impl From<serde_json::Error> for ApiErr {
     fn from(err: serde_json::Error) -> Self {
-        log::error!(target: "tin_actix_api_resp::error", "[SerdeJson] serde_json::Error: {:#?}", err);
+        log::error!(target: "tin_actix_api_resp::error", "[SerdeJson] serde_json::Error: {err:#?}");
         ApiErr::InternalServerError("Internal server error".into())
     }
 }
@@ -60,7 +58,7 @@ impl From<serde_json::Error> for ApiErr {
 #[cfg(feature = "chrono")]
 impl From<chrono::ParseError> for ApiErr {
     fn from(err: chrono::ParseError) -> Self {
-        log::error!(target: "tin_actix_api_resp::error", "[Chrono] chrono::ParseError: {:#?}", err);
+        log::error!(target: "tin_actix_api_resp::error", "[Chrono] chrono::ParseError: {err:#?}");
         ApiErr::InternalServerError("Internal server error".into())
     }
 }
